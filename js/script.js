78f7cb4b7158a1176ea6683d8d3eca49a5ac5684
paulomoreno <paulomorenoo@gmail.com>
2015-04-26 17:45:44 -0300

    Rodape + menu funcional e imagem do logo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
+
+    $("#show_login_cadastro").click(function(){
+    	$("#login_cadastro").toggleClass("hide",500);
+    });
+
 });
