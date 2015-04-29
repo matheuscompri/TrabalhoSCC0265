c75305e31777fae909d3882912be8949ecd6c65b
paulomoreno <paulomorenoo@gmail.com>
2015-04-29 19:04:57 -0300

    Pagina CadastroLugares com criação de multiplos campos extras

--- js/cadLugares.js
+++ js/cadLugares.js
@@ -1,4 +1,5 @@
 var fotos_index = 1;
+var campos_index = 1;
 
 $(document).ready(function(){
 	$('#add_mais_fotos').click(function(){
@@ -19,6 +20,35 @@ $(document).ready(function(){
 		});
 
 		fotos_index++;
+	});
+
+
+	$('#add_mais_campos').click(function(){
+		var campos = $(
+						'<div class="col s12 borda-on-small round-corner">'+
+						'    <div class="col s11">'+
+						'        <div class="input-field col l6 m6 s12">'+
+						'            <input id="campo_nome-'+campos_index+'" name="campo_nome0" type="text" class="validate">'+
+						'            <label for="campo_nome-'+campos_index+'">Nome do campo</label>'+
+						'        </div>'+
+						'        <div class="input-field col l6 m6 s12">'+
+						'           <input id="campo_valor-'+campos_index+'" name="campo_valor0" type="text" class="validate">'+
+						'            <label for="campo_valor-'+campos_index+'">Valor do campo</label>'+
+						'        </div>'+
+						'    </div>'+
+						'    <div class="col s1">'+
+						'        <a id="rmv_campos-'+campos_index+'" class="red-text" href=""><i class="mdi-action-highlight-remove small"></i></a>'+
+						'    </div>'+
+						'</div>');
+
+		campos.hide().insertBefore($(this).parent().parent()).slideDown()
+
+		$('#rmv_campos-'+campos_index).click(function(event){
+			campos.slideUp("normal", function() { $(this).remove(); } );
+			event.preventDefault();
+		});
+
+		campos_index++;
 	});
 
 });
