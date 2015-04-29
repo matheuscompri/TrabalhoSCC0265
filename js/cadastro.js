431d767f4f0fa04111737cb2cb29ce7d6ef684f9
matheuscompri <matheus.compri@gmail.com>
2015-04-29 09:40:37 -0300

    Algumas validacoes na pagina de cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
 
-
     $('#nome').mask("W", {
         translation: {
             'W': {
@@ -26,6 +25,64 @@ $(document).ready(function () {
             Materialize.toast('Caractere \'' + error.v + '\' Inválido para o Nome Completo!', 4000, 'rounded red darken-4 left');
             console.log("Digit: ", error.v, " is invalid for the position: ", error.p, ". We expect something like: ", error.e);
 
+        }
+    });
+
+    $('#cidade').mask("W", {
+        translation: {
+            'W': {
+                pattern: /[A-Za-z ]+/,
+                recursive: true
+            }
+        },
+        onKeyPress: function (name, event, currentField, options) {
+            $(currentField).val($(currentField).val().toUpperCase());
+        },
+        onInvalid: function (val, e, f, invalid, options) {
+            var error = invalid[0];
+            Materialize.toast('Caractere \'' + error.v + '\' Inválido para a Cidade!', 4000, 'rounded red darken-4 left');
+            console.log("Digit: ", error.v, " is invalid for the position: ", error.p, ". We expect something like: ", error.e);
+
+        }
+    });
+
+    $('#telefone').mask("(00) 00000-0000", {
+        onInvalid: function (val, e, f, invalid, options) {
+            var error = invalid[0];
+            Materialize.toast('Caractere \'' + error.v + '\' Inválido para o Telefone!', 4000, 'rounded red darken-4 left');
+            console.log("Digit: ", error.v, " is invalid for the position: ", error.p, ". We expect something like: ", error.e);
+
+        }
+    });
+
+    $('#senha').focusout(function () {
+        // Verificando se a senha esta vazia
+        if ($('#senha').val().trim() === '') {
+            Materialize.toast('A Senha é obrigatoria', 4000, 'rounded red darken-4 left');
+            $('#senha').addClass("invalid");
+        } else {
+            $('#senha').removeClass("invalid");
+        }
+
+    });
+    
+    $('#conf_senha').focusout(function () {
+        // Verificando se a confirmacao de senha esta vazia
+        if ($('#conf_senha').val().trim() === '') {
+            Materialize.toast('A Senha é obrigatoria', 4000, 'rounded red darken-4 left');
+            $('#conf_senha').addClass("invalid");
+        } else {
+            $('#conf_senha').removeClass("invalid");
+        }
+        
+        // Verificando se as senhas sao diferentes
+        if ($('#conf_senha').val() !== $('#senha').val()) {
+            Materialize.toast('Senhas diferentes', 4000, 'rounded red darken-4 left');
+            $('#senha').addClass("invalid");
+            $('#conf_senha').addClass("invalid");
+        } else {
+            $('#senha').removeClass("invalid");
+            $('#conf_senha').removeClass("invalid");
         }
     });
 });
