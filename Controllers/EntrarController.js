3c33e48385176a41ca02eb1d31293cf75c6f3672
paulomoreno <paulomorenoo@gmail.com>
2015-05-06 16:56:14 -0300

    Validação do Login através do angular

--- Controllers/EntrarController.js
+++ Controllers/EntrarController.js
@@ -4,13 +4,29 @@
         $scope.usuario = "";
         $scope.senha = "";
 
+        $scope.campoVazio = function(field){
+            return (field.$error.required && field.$dirty);
+        }
+
+        $scope.pattern = function(field){
+            return (field.$error.pattern && field.$dirty);
+        }
+
         $scope.entrar = function(usuario,senha){
-            var u = lugaresApi.login(usuario,senha); 
-            if(u){  
-                $rootScope.usuario = u;
-                $location.path( "/" );
+
+            if($scope.loginForm.$valid){
+                var u = lugaresApi.login(usuario,senha); 
+                if(u){  
+                    $rootScope.usuario = u;
+                    $location.path( "/" );
+                }else{
+                    $rootScope.usuario = false;
+                }
             }else{
-                $rootScope.usuario = false;
+                for (var i=0; i< $scope.loginForm.$error.required.length; i++){
+                    $scope.loginForm.$error.required[i].$setDirty();
+                }
             }
+
         };
     });
