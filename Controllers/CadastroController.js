c3082200792e30f59ea0386931787e6599d98a87
paulomoreno <paulomorenoo@gmail.com>
2015-05-06 16:46:27 -0300

    Melhoria na validação de data no cadastro de usuários

--- Controllers/CadastroController.js
+++ Controllers/CadastroController.js
@@ -13,13 +13,24 @@
             return (field.$error.pattern && field.$dirty);
         }
 
+        $scope.dataInvalida = function(field){
+            var valid = true;
+            var data_nascimento = field.$modelValue;
+            if (data_nascimento && data_nascimento.length === 10){
+                var data_nasc = new Date(data_nascimento.substr(6, 4),data_nascimento.substr(3, 2) - 1,data_nascimento.substr(0, 2));
+
+                if (data_nasc > new Date().addYears(-13)) {
+                    valid = false;
+                }
+            }
+            return !valid && field.$dirty;
+        }
+
         $scope.senhaInvalida = function(field){
             return (field.$error.pattern && field.$dirty);
         }
 
         $scope.senhaDiferente = function(field1,field2){
-            console.log(field1);
-            console.log(field2);
             return (field1.$modelValue !== field2.$modelValue) && (field1.$dirty) && field2.$dirty;
         }
 
@@ -27,29 +38,20 @@
 
             var valid = $scope.cadUsuario.$valid;
 
-            if (!data_nascimento || data_nascimento.length !== 10){
+            if ($scope.dataInvalida($scope.cadUsuario.data_nascimento))
                 valid = false;
-            }else{
-                var data_nasc = new Date(data_nascimento.substr(6, 4),data_nascimento.substr(3, 2) - 1,data_nascimento.substr(0, 2));
 
-                if (data_nasc > new Date().addYears(-13)) {
-                    valid = false;
-                    Materialize.toast('Você precisa ter mais de 13 anos para poder utilizar o site', 4000, 'rounded red darken-4 left');
-                    $('#data_nasc').addClass("invalid");
-                } else {
-                    $('#data_nasc').removeClass("invalid");
-                }
-            }
-
-            if (confirmar_senha !== senha){
+            if ($scope.senhaDiferente($scope.cadUsuario.senha,$scope.cadUsuario.confirmar_senha))
                 valid = false;
-            }
 
             if (!valid) {
                 //Deu ruim
-                alert("Deu ruim");
+                for (var i=0; i< $scope.cadUsuario.$error.required.length; i++){
+                    $scope.cadUsuario.$error.required[i].$setDirty();
+                }
+
             }else{
-                alert("Deu bom");
+                alert("Login com Sucesso!");
                 lugaresApi.novoUsuario(nome,data_nascimento,estado,cidade,telefone,email,senha);
 
                 var u = lugaresApi.login(email,senha); 
