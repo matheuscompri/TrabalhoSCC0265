61edef2f1eb0a19ee488839b02f8406cd4c52e33
matheuscompri <matheus.compri@gmail.com>
2015-05-07 10:52:17 -0300

    Validacao de alguns campos do cadastro de lugares

--- Controllers/CadastrarLugarController.js
+++ Controllers/CadastrarLugarController.js
@@ -1,4 +1,13 @@
     var trank = angular.module("trankApp");
 
     trank.controller("CadastrarLugarController", function ($scope) {
+        
+        $scope.campoVazio = function (field) {
+            return (field.$error.required && field.$dirty);
+        }
+
+        $scope.pattern = function (field) {
+            return (field.$error.pattern && field.$dirty);
+        }
+        
     });
