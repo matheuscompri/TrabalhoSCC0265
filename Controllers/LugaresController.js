5e5c87de553647e2dcc8bbe46607bbd782bc23c4
matheuscompri <matheus.compri@gmail.com>
2015-05-07 09:45:10 -0300

    Remocao de logs de telas ja finalizadas

--- Controllers/LugaresController.js
+++ Controllers/LugaresController.js
@@ -1,10 +1,6 @@
     var trank = angular.module("trankApp");
 
     trank.controller("LugaresController", function ($scope, categoria, lugares) {
-        //categoria = "america"
-        console.log(categoria);
-        console.log(lugares);
-
         $scope.categoria = categoria[0];
         $scope.lugares = lugares;
     });
