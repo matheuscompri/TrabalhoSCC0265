43d89d58c389b916e8f730b8cf48c7b61bc8374d
paulomoreno <paulomorenoo@gmail.com>
2015-05-07 12:26:12 -0300

    Widget para avaliação

--- Controllers/InicioController.js
+++ Controllers/InicioController.js
@@ -1,6 +1,12 @@
     var trank = angular.module("trankApp");
 
-    trank.controller("InicioController", function ($scope, lugaresApi) {
+    trank.controller("InicioController", function ($scope, $timeout, lugaresApi) {
+        $scope.$on("$viewContentLoaded", function () {
+	      $timeout(function() {
+	            initInicio();
+	      },0);
+        });
+
         $scope.lugares = lugaresApi.listaTodosLugares();
         $scope.categorias = lugaresApi.listarCategorias();
 
@@ -9,3 +15,20 @@
         }
 
     });
+
+
+    function initInicio(){
+
+    	$(".jrate").each(function(i,obj){
+    		var rating = $(this).data('rating');
+
+			$(this).raty({ 
+				starType: 'i',
+				score: rating,
+				readOnly: true,
+				halfShow: true,
+
+			});
+
+    	});
+    }
