eedce19f94685641692854bbc96b30e313f14ae1
paulomoreno <paulomorenoo@gmail.com>
2015-05-07 10:49:31 -0300

    Comentarios Fix

--- Controllers/LugarController.js
+++ Controllers/LugarController.js
@@ -5,6 +5,14 @@
         $scope.avaliacao = 4;
         $scope.usuario = $rootScope.usuario;
 
+        $rootScope.$watch('usuario', function( u ){
+            $scope.usuario = u;
+        });
+
+        $scope.$watch('usuario',function(u){
+            $rootScope.usuario = u;
+        });
+
         $scope.salvarComentario = function (username, comentario, lugarId) {
             lugaresApi.adicionarComentario(username, comentario, lugarId);
             $scope.comentario = "";
