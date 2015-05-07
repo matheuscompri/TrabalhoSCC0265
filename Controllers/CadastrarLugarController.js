abbfaaffd3789705fc42463f04901b292618f542
matheuscompri <matheus.compri@gmail.com>
2015-05-07 11:24:15 -0300

    Bug fix na tela de cadastro de lugares

--- Controllers/CadastrarLugarController.js
+++ Controllers/CadastrarLugarController.js
@@ -1,7 +1,13 @@
     var trank = angular.module("trankApp");
 
-    trank.controller("CadastrarLugarController", function ($scope) {
-        
+    trank.controller("CadastrarLugarController", function ($scope, $rootScope) {
+
+        $scope.usuario = $rootScope.usuario;
+
+        $rootScope.$watch('usuario', function (u) {
+            $scope.usuario = u;
+        });
+
         $scope.campoVazio = function (field) {
             return (field.$error.required && field.$dirty);
         }
@@ -9,5 +15,5 @@
         $scope.pattern = function (field) {
             return (field.$error.pattern && field.$dirty);
         }
-        
+
     });
