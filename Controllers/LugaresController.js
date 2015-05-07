42d15efb64baa59423287a88dfdfd2556a9c496e
paulomoreno <paulomorenoo@gmail.com>
2015-05-07 12:28:34 -0300

    Widget para avaliação na lista de lugares por categoria

--- Controllers/LugaresController.js
+++ Controllers/LugaresController.js
@@ -1,6 +1,12 @@
     var trank = angular.module("trankApp");
 
-    trank.controller("LugaresController", function ($scope, categoria, lugares) {
+    trank.controller("LugaresController", function ($scope, $timeout, categoria, lugares) {
+        $scope.$on("$viewContentLoaded", function () {
+	      $timeout(function() {
+	            initInicio();
+	      },0);
+        });
+
         $scope.categoria = categoria[0];
         $scope.lugares = lugares;
 
@@ -10,3 +16,18 @@
     });
 
 
+    function initLugares(){
+
+    	$(".jrate").each(function(i,obj){
+    		var rating = $(this).data('rating');
+
+			$(this).raty({ 
+				starType: 'i',
+				score: rating,
+				readOnly: true,
+				halfShow: true,
+
+			});
+
+    	});
+    }
