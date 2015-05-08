5319b39059724579339d84a3d0c77499d9f8a371
matheuscompri <matheus.compri@gmail.com>
2015-05-08 08:13:32 -0300

    Ajuste nos Graficos

--- Controllers/ComparativoController.js
+++ Controllers/ComparativoController.js
@@ -5,24 +5,14 @@ trank.controller("ComparativoController", function ($rootScope, $scope, lugaresA
     $scope.$on("$viewContentLoaded", function () {
         var data = {
             labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    fillColor: "rgba(220,220,220,0.5)",
-                    strokeColor: "rgba(220,220,220,0.8)",
-                    highlightFill: "rgba(220,220,220,0.75)",
-                    highlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-        },
-                {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.5)",
-                    strokeColor: "rgba(151,187,205,0.8)",
-                    highlightFill: "rgba(151,187,205,0.75)",
-                    highlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
+            datasets: [{
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+        }]
         };
 
         // Get context with jQuery - using jQuery's .get() method.
@@ -30,7 +20,26 @@ trank.controller("ComparativoController", function ($rootScope, $scope, lugaresA
 
         // This will get the first returned node in the jQuery collection.
         var myNewChart = new Chart(ctx);
-        myNewChart.Bar(data, { responsive: true });
+        myNewChart.Bar(data, {
+            responsive: true
+        });
         console.log(myNewChart);
+
+        $('#button').click(function () {
+
+            var data = {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.5)",
+                    strokeColor: "rgba(151,187,205,0.8)",
+                    highlightFill: "rgba(151,187,205,0.75)",
+                    highlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+        }]
+            };
+            myNewChart.Bar(data);
+
+        });
     });
 });
